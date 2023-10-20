846bb21d05a47456ab63dba7e64cc8fac5944a09
Tangereen1St <Tangereen1St@gmail.com>
2023-10-20 11:28:31 -0600

    V15

--- SHITBOX/Assets/Grabscript.cs
+++ SHITBOX/Assets/Grabscript.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Grabscript : MonoBehaviour
+using UnityEngine.InputSystem;
+public class GrabPhysics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputActionProperty grabInputSource;
+    public float radius = 0.1f;
+    public LayerMask grabLayer;
+
+    private FixedJoint fixedJoint;
+    private bool isGrabbing = false;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        bool isGrabButtonPressed = grabInputSource.action.ReadValue<float>() > 0.1f;
+
+        if (isGrabButtonPressed && !isGrabbing)
+        {
+            Collider[] nearbyColliders = Physics.OverlapSphere(transform.position, radius, grabLayer, QueryTriggerInteraction.Ignore);
+
+            if (nearbyColliders.Length > 0)
+            {
+                Rigidbody nearbyRigidbody = nearbyColliders[0].attachedRigidbody;
+
+                fixedJoint = gameObject.AddComponent<FixedJoint>();
+                fixedJoint.autoConfigureConnectedAnchor = false;
+
+                if (nearbyRigidbody)
+                {
+                    fixedJoint.connectedBody = nearbyRigidbody;
+                    fixedJoint.connectedAnchor = nearbyRigidbody.transform.InverseTransformPoint(transform.position);
+                }
+                else
+                {
+                    fixedJoint.connectedAnchor = transform.position;
+                }
+
+                isGrabbing = true;
+            }
+        }
+        else if (!isGrabButtonPressed && isGrabbing)
+        {
+            isGrabbing = false;
+
+            if (fixedJoint) ;
+            {
+                Destroy(fixedJoint);
+            }
+        }
     }
 }
